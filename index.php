fa03cd0de17429271676a1edc0faf74daaf23d5e
KumarShubham001 <shubhamrock6567@gmail.com>
2018-09-05 00:51:23 +0530

    Reinvention

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html lang="en" class="no-js">
+<head>
+
+    <title>Project_010</title>
+    
+    <meta charset="utf-8" />
+    <meta http-equiv="content-type" content="text/html" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- shortcut icon -->
+    <link rel="shortcut icon" href="" />
+
+    <!-- links for css designs -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"  />
+    <link href="css/style.css" rel="stylesheet" type="text/css"  />
+    
+    <!-- link for font awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+    
+</head>
+<body>
+    <div class="se-pre-con"></div>
+
+    <!-- links for js scripts -->
+    <script src="js/jquery-3.2.1.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="js/scroll.min.js" type="text/javascript"></script>
+    <script src="js/main.js" type="text/javascript"></script>
+</body>
+</html>
